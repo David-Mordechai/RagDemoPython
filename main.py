ea4c4941c1f44d529960f9f5fb978edd8cdbb62f
David Mordechai <david.mordechai79@gmail.com>
2025-05-09 18:17:52 +0300

    Add missing newline for code readability in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
+
 model = OllamaLLM(model="llama3.2", temperature=0.1)
 
 template = """
